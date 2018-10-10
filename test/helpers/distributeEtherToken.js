80ec7b70090fbe9c78d4e415beac58cd8660fc54
Mike Calvanese <mfcalvanese@gmail.com>
2018-10-10 15:05:13 -0400

    remove increment var from loop

--- test/helpers/distributeEtherToken.js
+++ test/helpers/distributeEtherToken.js
@@ -1,11 +1,9 @@
 export default async (accounts, etherToken, amount) => {
-  let account
-  for (var i in accounts) {
-    account = accounts[i]
-    const balance = (await etherToken.balanceOf(accounts[i])).toNumber()
+  for (let account of accounts) {
+    const balance = (await etherToken.balanceOf(account)).toNumber()
     if (balance == 0) {
       await etherToken.deposit({ value: amount, from: account })
-      console.log(`account:<${i}>:<${account}> bought ${amount/10**18} etherToken:<${etherToken.address}>`)
+      console.log(`account:<${account}> bought ${amount/10**18} etherToken:<${etherToken.address}>`)
     }
   }
   console.log('')
